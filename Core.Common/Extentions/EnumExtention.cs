34a618decf159fae2402f8126cdcdfeb843790c2
FazelMan <fazelman@ymail.com>
2018-11-04 18:51:13 +0330

    — Add ClientTypeEnum — Add and Implement UserAgent — Add and EnumExtentions

---
+++ Core.Common/Extentions/EnumExtention.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Core.Common.Extentions
+{
+    public static class EnumExtention
+    {
+        public static T GetEnum<T>(this string name)
+        {
+            var enumList = Enum.GetNames(typeof(T)).FirstOrDefault(x => x.ToLower() == name.ToLower());
+            var enumType = (T)Enum.Parse(typeof(T), enumList ?? throw new InvalidOperationException());
+            return enumType;
+        }
+    }
+}
---
+++ Core.Common/UserAgent/UserAgent.cs
@@ -0,0 +1,24 @@
+﻿using Core.Common.Enums;
+using Core.Common.Extentions;
+
+namespace Core.Common.UserAgent
+{
+    public static class UserAgent
+    {
+        public static ClientTypeEnum GetUserAgentClientType(string userAgent)
+        {
+            userAgent = userAgent.ToLower();
+
+            if (userAgent.Contains("android"))
+                userAgent = "android";
+
+            else if (userAgent.Contains("ipad") || userAgent.Contains("iphone") || userAgent.Contains("ios"))
+                userAgent = "ios";
+
+            else
+                userAgent = "web";
+
+            return userAgent.GetEnum<ClientTypeEnum>();
+        }
+    }
+}
