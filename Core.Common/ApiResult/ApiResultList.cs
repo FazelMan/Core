6d586606976e3e8aa48324c164f0b31b11ed0fe2
FazelMan <fazelman@ymail.com>
2018-11-28 15:19:28 +0330

    — add ApiResultList and ApiResultList<T>

--- Core.Common/ApiResult/ApiResultList.cs
+++ Core.Common/ApiResult/ApiResultList.cs
@@ -2,17 +2,21 @@
 
 namespace Core.Common.ApiResult
 {
-    public class ApiResult
+    public class ApiResultList
     {
         public string Message { get; set; }
         public object Result { get; set; }
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
         public DateTime ResultDate => DateTime.Now;
     }
 
-    public class ApiResult<T>
+    public class ApiResultList<T>
     {
         public string Message { get; set; }
         public T Result { get; set; }
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
         public DateTime ResultDate => DateTime.Now;
     }
 }
