e501277708389df602abc1da89ca52a1a36cbd5d
FazelMan <fazelman@ymail.com>
2018-12-29 15:10:11 +0330

    add NUnit test library

---
+++ FazelMan.Core.Common.Tests/Extentions/RegexExtentionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FazelMan.Core.Common.Enums;
+using FazelMan.Core.Common.Extentions;
+using FazelMan.Core.Tests;
+using NUnit.Framework;
+
+namespace FazelMan.Core.Common.Tests.Extentions
+{
+    [TestFixture]
+    public class RegexExtentionTests
+    {
+        [Test]
+        public void Must_true_phoneNumber_regex()
+        {
+            "09334443365".IsValidRegex(RegexType.IranPhoneNumber).ShouldBeTrue();
+            "+989334443365".IsValidRegex(RegexType.IranPhoneNumber).ShouldBeTrue();
+        }
+
+        [Test]
+        public void Must_true_email_regex()
+        {
+            "fazelman@ymail.com".IsValidRegex(RegexType.Email).ShouldBeTrue();
+        }
+    }
+}
