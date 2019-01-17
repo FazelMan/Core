ce2c7c8d7e509c312d5a7373af7281e663170faf
FazelMan <fazelman@ymail.com>
2019-01-18 03:17:55 +0330

    — add RemvoeVersionParameter filter — add SetVersionInPaths filter

---
+++ FazelMan.Swagger/OperationFilters/RemoveVersionParameters.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FazelMan.Swagger.OperationFilters
+{
+    public class RemoveVersionParameters : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            var versionParameter = operation.Parameters.Single(p => p.Name == "version");
+            operation.Parameters.Remove(versionParameter);
+        }
+    }
+}
