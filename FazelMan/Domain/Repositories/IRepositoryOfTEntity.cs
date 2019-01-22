55d27b4f3b5ee8ea3e2857ac826f4e13807080bd
Fazel <Fazel@FAZELI-PC>
2019-01-22 20:33:43 +0330

    cleanUp

--- FazelMan/Domain/Repositories/IRepositoryOfTEntity.cs
+++ FazelMan/Domain/Repositories/IRepositoryOfTEntity.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-using FazelMan.Application.Services.ApiResult;
-using FazelMan.Domain.Entities;
-using FazelMan.Dto.Api;
+﻿using FazelMan.Domain.Entities;
 
 namespace FazelMan.Domain.Repositories
 {
