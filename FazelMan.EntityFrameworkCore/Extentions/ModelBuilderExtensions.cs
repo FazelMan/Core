f65fda14460d67f4d04c41fba7017094ffa9b5d0
Fazel <Fazel@FAZELI-PC>
2019-01-27 14:28:08 +0330

    add ModelBuilderExtensions

---
+++ FazelMan.EntityFrameworkCore/Extentions/ModelBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using FazelMan.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Remotion.Linq.Parsing.ExpressionVisitors;
+
+namespace FazelMan.EntityFrameworkCore.Extentions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyDecimalPrecision(this ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model
+                .GetEntityTypes()
+                .SelectMany(t => t.GetProperties())
+                .Where(p => p.ClrType == typeof(decimal)))
+            {
+                property.Relational().ColumnType = "decimal(18, 6)";
+            }
+        }
+
+        public static void ApplyQueryFilter(this ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes()
+                .Where(e => typeof(ISoftDelete).IsAssignableFrom(e.ClrType)))
+            {
+                modelBuilder
+                    .Entity(entityType.ClrType)
+                    .HasQueryFilter(ConvertFilterExpression<ISoftDelete>(e => !e.IsRemoved, entityType.ClrType));
+            }
+        }
+
+        private static LambdaExpression ConvertFilterExpression<TInterface>(
+            Expression<Func<TInterface, bool>> filterExpression,
+            Type entityType)
+        {
+            var newParam = Expression.Parameter(entityType);
+            var newBody = ReplacingExpressionVisitor.Replace(filterExpression.Parameters.Single(), newParam, filterExpression.Body);
+
+            return Expression.Lambda(newBody, newParam);
+        }
+    }
+}
